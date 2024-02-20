c49fe1326d1693178f9d0b6b326937129adc8d64
Seb Tiburzio <sebtiburzio@gmail.com>
2024-02-20 18:05:27 +0100

    Add set motor offsets service

--- helix_transmission/helix_transmission/tendon_transmission.py
+++ helix_transmission/helix_transmission/tendon_transmission.py
@@ -40,9 +40,11 @@ class TendonTransmissionNode(Node):
                 if 'motor_offsets' in data:
                     self.MOTOR_OFFSETS = data['motor_offsets']
                 else:
-                    self.get_logger().info('No offset data found in calibration file, offsets set to 0.')
+                    self.get_logger().info('No offset data found in calibration file, offsets set to 0')
         except (FileNotFoundError, yaml.YAMLError, TypeError):
-            self.get_logger().info('Failed to load calibration file, offsets set to 0.')
+            self.get_logger().info('Failed to load calibration file, offsets set to 0')
+
+        self.last_motor_joint_positions = None
 
         # Subscription/publication for motor<->tendon transmission
         self.tendon_command_sub = self.create_subscription(
@@ -77,19 +79,24 @@ class TendonTransmissionNode(Node):
         
         # Client/service for setting holding current
         client_cb_group = MutuallyExclusiveCallbackGroup()
+
         self.controller_switch_cli = self.create_client(
             SwitchController, '/controller_manager/switch_controller', callback_group=client_cb_group)
 
         service_cb_group = MutuallyExclusiveCallbackGroup()
+
         self.set_holding_current_srv = self.create_service(
-            Trigger, '~/set_holding_current_srv', self.set_holding_current_cb, callback_group=service_cb_group)
+            Trigger, '~/set_holding_current', self.set_holding_current_cb, callback_group=service_cb_group)
+        
+        self.set_motor_offsets_srv = self.create_service(
+            Trigger, '~/set_motor_offsets', self.set_motor_offsets_cb, callback_group=service_cb_group)
 
     # Callbacks for motor<->tendon transmission
     def tendon_to_motor_command_cb(self, msg):
         command = np.array(msg.data, dtype=np.float64)
         command_limited = np.clip(command, self.TENDON_LIMITS[0], self.TENDON_LIMITS[1])
         if not np.array_equal(command, command_limited):
-            self.get_logger().info('Some tendon commands exceeded limits and were clipped.')
+            self.get_logger().info('Some tendon commands exceeded limits and were clipped')
         motor_command = Float64MultiArray()
         motor_command.data = ((command_limited * self.MOTOR_ORIENTS) \
                                / self.PULLEY_RADIUS + self.MOTOR_OFFSETS).tolist()
@@ -99,17 +106,18 @@ class TendonTransmissionNode(Node):
         tendon_state = JointState()
         motor_names = sorted(msg.name)
         motor_angs = [msg.position[msg.name.index(motor_name)] for motor_name in motor_names]
+        self.last_motor_joint_positions = motor_angs
         motor_angvels = [msg.velocity[msg.name.index(motor_name)] for motor_name in motor_names]
         motor_currents = [msg.effort[msg.name.index(motor_name)] for motor_name in motor_names]
         tendon_state.name = motor_names
-        tendon_state.position = ((np.array(motor_angs, dtype=np.float64) + self.MOTOR_OFFSETS) \
+        tendon_state.position = ((np.array(motor_angs, dtype=np.float64) - self.MOTOR_OFFSETS) \
                                  * self.PULLEY_RADIUS * self.MOTOR_ORIENTS).tolist()
         tendon_state.velocity = (np.array(motor_angvels, dtype=np.float64) \
                                  * self.PULLEY_RADIUS * self.MOTOR_ORIENTS).tolist()
         tendon_state.effort = motor_currents
         self.tendon_state_pub.publish(tendon_state)
 
-    # Callback for setting holding current
+    # Callbacks for calibration
     def set_holding_current_cb(self, request, response):
         while not self.controller_switch_cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('Waiting for controller switch service')
@@ -121,14 +129,29 @@ class TendonTransmissionNode(Node):
         while self.executor.spin_until_future_complete(controller_switch_future):
             self.get_logger().info("Waiting for controller switch to complete")
         if controller_switch_future.result().ok == False:
-            self.get_logger().error('Failed to switch to effort controller.')
+            self.get_logger().error('Failed to switch to effort controller')
             response.success = False
-            response.message = 'Failed to switch to effort controller.'
+            response.message = 'Failed to switch to effort controller'
             return response
         self.motor_effort_command_pub.publish(
             Float64MultiArray(data = self.HOLDING_CURRENT * self.MOTOR_ORIENTS))
         response.success = True
         return response
+    
+    def set_motor_offsets_cb(self, request, response):
+        try:
+            with open(self.get_parameter('tendon_calib_file_path').value + '.bak', 'w') as backup:
+                yaml.dump({'motor_offsets': self.MOTOR_OFFSETS}, backup)
+            with open(self.get_parameter('tendon_calib_file_path').value, 'w') as file:
+                self.MOTOR_OFFSETS = self.last_motor_joint_positions
+                yaml.dump({'motor_offsets': self.MOTOR_OFFSETS}, file)
+            response.success = True
+            return response
+        except (FileNotFoundError, yaml.YAMLError, TypeError):
+            self.get_logger().info('Failed to write motor offsets to file')
+            response.success = False
+            response.message = 'Failed to write motor offsets to file'
+            return response
 
 
 def main(args=None):
