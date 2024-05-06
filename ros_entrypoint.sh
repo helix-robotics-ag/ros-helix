07b228d9c900e689129871049c6531008cd3d72e
Kai <kaijunge21@gmail.com>
2024-05-06 18:15:45 +0200

    Add input group and add the ros group into it.

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 id -u ros &>/dev/null || adduser --quiet --disabled-password --gecos '' --uid ${UID:=1000} ros
 
+getent group input || groupadd -g 107 input
+usermod -aG input ros
+
 source /opt/ros/${ROS_DISTRO}/setup.bash
 spacenavd
 
