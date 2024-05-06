a370522f4b2b69cb59b6ddbade2ae6e12df89dcb
Kai <kaijunge21@gmail.com>
2024-05-06 18:52:51 +0200

    Removed group addition as it was not needed.

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -3,9 +3,6 @@ set -e
 
 id -u ros &>/dev/null || adduser --quiet --disabled-password --gecos '' --uid ${UID:=1000} ros
 
-getent group input || groupadd -g 107 input
-usermod -aG input ros
-
 source /opt/ros/${ROS_DISTRO}/setup.bash
 spacenavd
 
