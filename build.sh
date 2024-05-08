29f17e6141d8450f92fb1b4fb97a7e09559913cb
Max Polzin <polzin.max@gmail.com>
2024-05-08 21:52:40 +0200

    Update build progress to plain output.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export BUILDKIT_PROGRESS=plain
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 docker compose -f $SCRIPT_DIR/docker-compose.yml build 
