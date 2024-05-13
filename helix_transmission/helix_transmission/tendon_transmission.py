f1ef23ad888d3af67780280cd46a245ed4d16414
Seb Tiburzio <sebtiburzio@gmail.com>
2024-05-13 16:01:41 +0200

    Transfer message timestamp from motor controller to tendon transmission

--- helix_transmission/helix_transmission/tendon_transmission.py
+++ helix_transmission/helix_transmission/tendon_transmission.py
@@ -129,12 +129,13 @@ class TendonTransmissionNode(Node):
         self.tendon_to_motor_command_pub.publish(motor_command)
 
     def motor_state_cb(self, msg):
-        tendon_state = JointState()
         motor_names = sorted(msg.name)
         motor_angs = [msg.position[msg.name.index(motor_name)] for motor_name in motor_names]
         self.last_motor_joint_positions = np.array(motor_angs, dtype=np.float64)
         motor_angvels = [msg.velocity[msg.name.index(motor_name)] for motor_name in motor_names]
         motor_currents = [msg.effort[msg.name.index(motor_name)] for motor_name in motor_names]
+        tendon_state = JointState()
+        tendon_state.header.stamp = msg.header.stamp
         tendon_state.name = motor_names
         tendon_state.position = ((np.array(motor_angs, dtype=np.float64) - self.MOTOR_OFFSETS) \
                                  * self.PULLEY_RADIUS * self.MOTOR_ORIENTS).tolist()
